c7e11e7b6e20aaedb2f0140df9ce58940ed48078
Atiq Ur Rehman <mathcity@gmail.com>
2026-05-16 15:11:52 +0200

    Translation update (ur)

---
+++ lang/ur/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Atiq Ur Rehman <mathcity@gmail.com>
+ */
+$lang['vs_tgzno']              = 'ڈوکووکی کے تازہ ترین ورژن کا تعین نہیں کیا جا سکا۔';
+$lang['vs_localno']            = 'یہ واضح نہیں ہے کہ آپ کا موجودہ ورژن کتنا پرانا ہے، خود سے اپ گریڈ  کرنے کی سفارش کی جاتی ہے۔';
